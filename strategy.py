14cc2e19a03af96dc7eea97130c0cf79bb1204d6
saharshaul <65076361+saharshaul@users.noreply.github.com>
2020-10-04 16:13:02 +0300

    Add files via upload

--- strategy.py
+++ strategy.py
@@ -1,7 +1,25 @@
-
-class BaseStrategy:
-
-	def play(self):
+import random
 
 
-	def perform_strategy(self, counter):
+class Automatic_BaseStrategy(BaseStategy):
+  def perform_startegy(self, counter):
+      rnd = random.randint(100)
+      return envelopes[rnd]
+
+
+class N_max_srategy(BaseStategy):
+  def __init__(self, n):
+    self.n = n
+
+  def perform_startegy(self, counter):
+      pass
+
+
+class More_then_N_percent_group_srategy(BaseStategy):
+    def __init__(self, percent):
+        self.percent = percent
+
+    def perform_startegy(self, counter):
+        pass
+
+
